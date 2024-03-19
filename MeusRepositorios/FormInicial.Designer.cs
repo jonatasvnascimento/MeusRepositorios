fba850a7110839bef9cee0ecdaa639c36bd0490d
Jonatas Nascimento <56093381+jonatasvnascimento@users.noreply.github.com>
2024-03-19 20:01:00 -0300

    feat: adicionando layout novo e form dentro de form

--- MeusRepositorios/FormInicial.Designer.cs
+++ MeusRepositorios/FormInicial.Designer.cs
@@ -28,21 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panelMenu = new Panel();
+            btnFavorites = new Button();
+            btnMyRepository = new Button();
+            panelLogo = new Panel();
+            label1 = new Label();
+            panelTitleBar = new Panel();
+            lblTitle = new Label();
+            panelDesktopPane = new Panel();
+            panelMenu.SuspendLayout();
+            panelLogo.SuspendLayout();
+            panelTitleBar.SuspendLayout();
             SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(51, 51, 76);
+            panelMenu.Controls.Add(btnFavorites);
+            panelMenu.Controls.Add(btnMyRepository);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(233, 782);
+            panelMenu.TabIndex = 0;
+            // 
+            // btnFavorites
+            // 
+            btnFavorites.Dock = DockStyle.Top;
+            btnFavorites.FlatAppearance.BorderSize = 0;
+            btnFavorites.FlatStyle = FlatStyle.Flat;
+            btnFavorites.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnFavorites.ForeColor = Color.White;
+            btnFavorites.Location = new Point(0, 160);
+            btnFavorites.Name = "btnFavorites";
+            btnFavorites.Size = new Size(233, 54);
+            btnFavorites.TabIndex = 2;
+            btnFavorites.Text = "Favoritos";
+            btnFavorites.UseVisualStyleBackColor = true;
+            btnFavorites.Click += btnFavorites_Click;
+            // 
+            // btnMyRepository
+            // 
+            btnMyRepository.Dock = DockStyle.Top;
+            btnMyRepository.FlatAppearance.BorderSize = 0;
+            btnMyRepository.FlatStyle = FlatStyle.Flat;
+            btnMyRepository.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnMyRepository.ForeColor = Color.White;
+            btnMyRepository.Location = new Point(0, 106);
+            btnMyRepository.Name = "btnMyRepository";
+            btnMyRepository.Size = new Size(233, 54);
+            btnMyRepository.TabIndex = 1;
+            btnMyRepository.Text = "Meus Repositorios";
+            btnMyRepository.UseVisualStyleBackColor = true;
+            btnMyRepository.Click += btnMyRepository_Click;
+            // 
+            // panelLogo
+            // 
+            panelLogo.BackColor = Color.FromArgb(39, 39, 58);
+            panelLogo.Controls.Add(label1);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(233, 106);
+            panelLogo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(64, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 18);
+            label1.TabIndex = 0;
+            label1.Text = "MyRepository";
+            // 
+            // panelTitleBar
+            // 
+            panelTitleBar.BackColor = Color.FromArgb(51, 51, 76);
+            panelTitleBar.Controls.Add(lblTitle);
+            panelTitleBar.Dock = DockStyle.Top;
+            panelTitleBar.Location = new Point(233, 0);
+            panelTitleBar.Name = "panelTitleBar";
+            panelTitleBar.Size = new Size(873, 106);
+            panelTitleBar.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            lblTitle.Anchor = AnchorStyles.None;
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblTitle.ForeColor = Color.White;
+            lblTitle.Location = new Point(379, 39);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(68, 24);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "HOME";
+            // 
+            // panelDesktopPane
+            // 
+            panelDesktopPane.Dock = DockStyle.Fill;
+            panelDesktopPane.Location = new Point(233, 106);
+            panelDesktopPane.Name = "panelDesktopPane";
+            panelDesktopPane.Size = new Size(873, 676);
+            panelDesktopPane.TabIndex = 2;
             // 
             // FormInicial
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(800, 450);
-            FormBorderStyle = FormBorderStyle.None;
+            ClientSize = new Size(1106, 782);
+            Controls.Add(panelDesktopPane);
+            Controls.Add(panelTitleBar);
+            Controls.Add(panelMenu);
             Name = "FormInicial";
             Text = "Form1";
             Load += FormInicial_Load;
+            panelMenu.ResumeLayout(false);
+            panelLogo.ResumeLayout(false);
+            panelLogo.PerformLayout();
+            panelTitleBar.ResumeLayout(false);
+            panelTitleBar.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelMenu;
+        private Button btnMyRepository;
+        private Panel panelLogo;
+        private Label label1;
+        private Button btnFavorites;
+        private Panel panelTitleBar;
+        private Label lblTitle;
+        private Panel panelDesktopPane;
     }
 }
--- MeusRepositorios/FormInicial.cs
+++ MeusRepositorios/FormInicial.cs
@@ -1,6 +1,8 @@
 using MeusRepositorios.Domain.Model;
 using MeusRepositorios.Service.Interface;
 using MeusRepositorios.Service.Services;
+using MeusRepositorios.Ultils;
+using System.Runtime.InteropServices;
 
 namespace MeusRepositorios
 {
@@ -8,6 +10,12 @@ namespace MeusRepositorios
     {
         private MyRepositoryService _myRepositoryService;
         public List<MyRepository> myRepository { get; set; }
+
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+        private Form activeForm;
+
         public FormInicial()
         {
             InitializeComponent();
@@ -16,7 +24,80 @@ namespace MeusRepositorios
 
         private void FormInicial_Load(object sender, EventArgs e)
         {
-            myRepository = _myRepositoryService.Get().ToList();
+            //myRepository = _myRepositoryService.Get().ToList();
+        }
+
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        //Methods
+        private Color SelectThemeColor()
+        {
+            string color = ThemeColor.ColorList[0];
+            return ColorTranslator.FromHtml(color);
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    //panelTitleBar.BackColor = color;
+                    //panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    ThemeColor.PrimaryColor = color;
+                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    //btnCloseChildForm.Visible = true;
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelDesktopPane.Controls.Add(childForm);
+            this.panelDesktopPane.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lblTitle.Text = childForm.Text;
+        }
+
+
+        private void btnMyRepository_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new App.Forms.FormMyRepository(), sender);
+        }
+
+        private void btnFavorites_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new App.Forms.FormFavorites(), sender);
         }
     }
 }
